82a855c27493c17d369ce044c4cec7720a98f4b9
Ryan Hornung <rmh286@cornell.edu>
2018-09-03 01:22:57 -0400

    Cylinder generation start

--- a1/src/meshgen/MeshGen.java
+++ a1/src/meshgen/MeshGen.java
@@ -26,6 +26,135 @@ public class MeshGen {
 
     }
 
+    private static OBJMesh generateCylinder(int n) {
+        OBJMesh mesh = new OBJMesh();
+        // (2 * Math.PI) / n / 2
+        // vertices on the top and bottom are offset so each step is a half angle
+        double deltaAngle = Math.PI / n;
+        double currAngle = 0
+
+        Vector3 topVertex = Vector3(0, 1, 0);
+        Vector3 bottomVertex = Vector3(0, -1, 0);
+        //Vector2 topVertexUV = Vector2();
+        //Vector2 bottomVertexUV = Vertex2();
+        mesh.positions.add(topVertex);
+        int topVertexPos = mesh.positions.size() - 1;
+        mesh.positions.add(bottomVertex);
+        int bottomVertexPos = mesh.positions.size() - 1;
+        mesh.normals.add(topVertex);
+        int topNormalPos = mesh.normals.size() -1;
+        mesh.normals.add(bottomVertex);
+        int bottomNormalPos = mesh.normals.size() - 1;
+
+        // start at seam (z = 1)
+        Vector3 v1;
+        Vector3 v2;
+        Vector3 v3;
+        Vector3 v1Normal;
+        Vector3 v2Normal;
+        Vector3 v3Normal;
+        int v1Pos;
+        int v2Pos;
+        int v3Pos;
+        int v1NormalPos;
+        int v2NormalPos;
+        int v3NormalPos;
+        v1 = new Vector3(Math.sin(currAngle), 1, Math.cos(currAngle));
+        v1Normal = new Vector3(Math.sin(currAngle), 0, Math.cos(currAngle)).normalize();
+        currAngle += deltaAngle;
+        v2 = new Vector3(Math.sin(currAngle), -1, Math.cos(currAngle));
+        v2Normal = new Vector3(Math.sin(currAngle), 0, Math.cos(currAngle)).normalize();
+        mesh.positions.add(v1);
+        v1Pos = mesh.positions.size() - 1;
+        mesh.positions.add(v2);
+        v2Pos = mesh.positions.size() - 1;
+        mesh.normals.add(v1Normal);
+        v1NormalPos = mesh.normals.size() - 1;
+        mesh.normals.add(v2Normal);
+        v2NormalPos = mesh.normals.size() - 1;
+
+
+        OBJFace face;
+
+        for (int i = 0; i < n-1; i++) {
+            // add next vertex on upper cap (+y)
+            currAngle += deltaAngle;
+            v3 = new Vector3(Math.sin(currAngle), 1, Math.cos(currAngle));
+            v3Normal = new Vector3(Math.sin(currAngle), 0, Math.cos(currAngle)).normalize();
+            mesh.positions.add(v3);
+            v3Pos = mesh.positions.size() - 1;
+            mesh.normals.add(v3Normal);
+            v3NormalPos = mesh.normals.size() - 1;
+
+            // form a new face on upper cap (+y)
+            face = new OBJFace(3, true, true);
+            face.positions[0] = v1Pos;
+            face.positions[1] = v3Pos;
+            face.positions[2] = topVertexPos;
+            // figure out uv coordinates
+            face.normals[0] = topNormalPos;
+            face.normals[1] = topNormalPos;
+            face.normals[3] = topNormalPos;
+            mesh.faces.add(face);
+
+            // form a new face on side of cylinder pointing down (-y)
+            face = new OBJFace(3, true, true);
+            face.positions[0] = v1Pos;
+            face.positions[1] = v2Pos;
+            face.positions[2] = v3Pos;
+            // figure out uv coordinates
+            face.normals[0] = v1NormalPos;
+            face.normals[1] = v2NormalPos;
+            face.normals[3] = v3NormalPos;
+            mesh.faces.add(face);
+
+            int v1Pos = v2Pos;
+            int v2Pos = v3Pos;
+            int v1NormalPos = v2NormalPos;
+            int v2NormalPos = v3NormalPos;
+
+            // add next vertex on lower cap (-y)
+            currAngle += deltaAngle;
+            v3 = new Vector3(Math.sin(currAngle), -1, Math.cos(currAngle));
+            v3Normal = new Vector3(Math.sin(currAngle), 0, Math.cos(currAngle)).normalize();
+            mesh.positions.add(v3);
+            v3Pos = mesh.positions.size() - 1;
+            mesh.normals.add(v3Normal);
+            v3NormalPos = mesh.normals.size() - 1;
+
+            // form a new face on lower cap (-y)
+            face = new OBJFace(3, true, true);
+            face.positions[0] = v3Pos;
+            face.positions[1] = v1Pos;
+            face.positions[2] = bottomVertexPos;
+            // figure out uv coordinates
+            face.normals[0] = bottomNormalPos;
+            face.normals[1] = bottomNormalPos;
+            face.normals[3] = bottomNormalPos;
+            mesh.faces.add(face);
+
+            // form a new face on side of cylinder pointing up (+y)
+            face = new OBJFace(3, true, true);
+            face.positions[0] = v3Pos;
+            face.positions[1] = v2Pos;
+            face.positions[2] = v1Pos;
+            // figure out uv coordinates
+            face.normals[0] = v3NormalPos;
+            face.normals[1] = v2NormalPos;
+            face.normals[3] = v1NormalPos;
+            mesh.faces.add(face);
+
+            int v1Pos = v2Pos;
+            int v2Pos = v3Pos;
+            int v1NormalPos = v2NormalPos;
+            int v2NormalPos = v3NormalPos;
+        }
+
+        // stitch final triangles
+
+        return null;
+    }
+
     private static void printUsage() {
         StringBuilder string = new StringBuilder();
         string.append("Usage:  ");
