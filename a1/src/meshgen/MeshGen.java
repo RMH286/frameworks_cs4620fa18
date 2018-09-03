dcbbcc2a9bd6fa8aa9e82720df709a43b836d620
root <root@localhost.localdomain>
2018-09-03 12:26:49 -0400

    cylinder position first draft

--- a1/src/meshgen/MeshGen.java
+++ a1/src/meshgen/MeshGen.java
@@ -155,6 +155,88 @@ public class MeshGen {
         return null;
     }
 
+    //convert spherical to cartesian corrdinates
+    private static Vector3 sphereToCart(Vector3 sph){
+        float r = sph.get(0);
+        float theta = sph.get(1);
+        float phi = sph.get(2);
+        return new Vector3((Math.sin(theta)*r),(Math.cos(phi)*r),(Math.cos(theta)*r));
+    }
+
+    private static OBJMesh generateSphere(int n, int m) {
+        OBJMesh mesh = new OBJMesh();
+        
+        //phi delta
+        double dp = Math.PI/m;
+        //theta delta
+        double dt = Math.PI/n;
+        //start at meridian and south most lattitude
+        //phi start
+        double cp = Math.PI*((m-1)/m)
+        //theta start
+        double ct = 0.0;
+
+        //keep track of the last row of position vectors so they can be reused.
+        int[] lastP = new int[n-1];
+
+        //create southmost latt of vertecies
+        for(int i = 0; i < n; i++){
+            //posistion
+            mesh.positions.add(sphereToCart(new Vector3(1,ct,cp)));
+            lastP[i] = mesh.positions.size()-1;
+            pS[i] = mesh.positions.size()-1;
+            ct += dt;
+        }
+
+        //create faces
+        //from the southmost latt to north most latt with two triangles per square
+        for(int i = 0; i < (m/2)-1; i++){
+            //update cordinates
+            ct = 0.0;
+            cp += dp;
+            mesh.positions.add(sphereToCart(new Vector3(1,ct,cp)));
+            for(int j = 0; j < n; j++){
+
+                //update vector positions
+                int v1;
+                int v2;
+                int v3;
+                int v4;
+                v2 = lastP[j];
+                v4 = lastP[j+1];
+                v1 = mesh.positions.size()-1;
+                ct += dt;
+                mesh.positions.add(sphereToCart(new Vector3(1,ct,cp)));
+                v3 = mesh.positions.size()-1;
+
+                //update lastP
+                lastP[j] = v1;
+                if(j+1 == n) lastP[j+1] = v3;
+
+                //create face
+                face1 = new OBJFace(3, true, true);
+                face1.positions[0] = v1;
+                face1.positions[1] = v2;
+                face1.positions[2] = v4;
+
+                face2 = new OBJFace(3, true, true);
+                face2.positions[0] = v1;
+                face2.positions[1] = v4;
+                face2.positions[2] = v3;
+
+                mesh.faces.add(face1);
+                mesh.faces.add(face2);
+
+                ct += dt;
+
+            }
+        }
+
+
+
+        return mesh;
+    }
+
     private static void printUsage() {
         StringBuilder string = new StringBuilder();
         string.append("Usage:  ");
