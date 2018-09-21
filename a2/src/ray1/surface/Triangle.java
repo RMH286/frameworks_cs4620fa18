c929eb638e7bb492155b381f2b1b6cf7d6f1df20
Ryan Hornung <rmh286@cornell.edu>
2018-09-21 01:51:58 -0400

    fix triangle intersect

--- a2/src/ray1/surface/Triangle.java
+++ a2/src/ray1/surface/Triangle.java
@@ -75,7 +75,12 @@ public class Triangle extends Surface {
     double l = v0.z - rayIn.origin.z;
 
     double m = a*(e*i - h*f) + b*(g*f - d*i) + c*(d*h - e*g);
-    double t = -(f*(a*k - j*b) + h*(j*c - a*l) + d*(b*l - k*c)) / m;
+    double t = -(f*(a*k - j*b) + e*(j*c - a*l) + d*(b*l - k*c)) / m;
+
+    if (t < rayIn.start || t > rayIn.end) {
+      return false;
+    }
+
     double gamma = (i*(a*k - j*b) + h*(j*c - a*l) + g*(b*l - k*c)) / m;
 
     if (gamma < 0 || gamma > 1) {
@@ -89,54 +94,55 @@ public class Triangle extends Surface {
     }
 
     // USE BARYCENTRIC COORDINATES TO FILL IN OUTRECORD
-    //alpha = 1-beta-gamma to find relationship between intersect 
-    //point to all vertices.
+    // alpha = 1-beta-gamma to find relationship between intersect 
+    // point to all vertices.
     double alpha = 1-beta-gamma;
     
-    //calculate normal
+    // calculate normal
     Vector3d normal;
     if(face.hasNormals()) {
-    	Vector3 n0s = this.owner.getMesh().getNormal(face,0);
-    	Vector3 n1s = this.owner.getMesh().getNormal(face,1);
-    	Vector3 n2s = this.owner.getMesh().getNormal(face,2);
-    	Vector3d n0uw = new Vector3d(n0s.get(0),n0s.get(1),n0s.get(2));
-    	Vector3d n1uw = new Vector3d(n1s.get(0),n1s.get(1),n1s.get(2));
-    	Vector3d n2uw = new Vector3d(n2s.get(0),n2s.get(1),n2s.get(2));
-    	//compute intersect normal by adding together weighted normals
-    	Vector3d n0 = n0uw.mul(alpha);
-    	Vector3d n1 = n1uw.mul(beta);
-    	Vector3d n2 = n2uw.mul(gamma);
-    	normal = n0.add(n1).add(n2);
+      Vector3 n0s = this.owner.getMesh().getNormal(face,0);
+      Vector3 n1s = this.owner.getMesh().getNormal(face,1);
+      Vector3 n2s = this.owner.getMesh().getNormal(face,2);
+      Vector3d n0uw = new Vector3d(n0s.get(0),n0s.get(1),n0s.get(2));
+      Vector3d n1uw = new Vector3d(n1s.get(0),n1s.get(1),n1s.get(2));
+      Vector3d n2uw = new Vector3d(n2s.get(0),n2s.get(1),n2s.get(2));
+      // compute intersect normal by adding together weighted normals
+      Vector3d n0 = n0uw.mul(alpha);
+      Vector3d n1 = n1uw.mul(beta);
+      Vector3d n2 = n2uw.mul(gamma);
+      normal = n0.add(n1).add(n2);
     }
     else normal = new Vector3d(norm.get(0),norm.get(1),norm.get(2));
-    
-    //calculate Texture value.
+
+    // calculate Texture value.
     Vector2d texture;
     if(face.hasUVs()){
-    	Vector2 t0s = this.owner.getMesh().getUV(face,0);
-    	Vector2 t1s = this.owner.getMesh().getUV(face,1);
-    	Vector2 t2s = this.owner.getMesh().getUV(face,2);
-    	Vector2d t0uw = new Vector2d(t0s.get(0),t0s.get(1));
-    	Vector2d t1uw = new Vector2d(t1s.get(0),t1s.get(1));
-    	Vector2d t2uw = new Vector2d(t2s.get(0),t2s.get(1));
-    	Vector2d t0 = t0uw.mul(alpha);
-    	Vector2d t1 = t1uw.mul(beta);
-    	Vector2d t2 = t2uw.mul(gamma);
-    	
-    	texture = t0.add(t1).add(t2);
+      Vector2 t0s = this.owner.getMesh().getUV(face,0);
+      Vector2 t1s = this.owner.getMesh().getUV(face,1);
+      Vector2 t2s = this.owner.getMesh().getUV(face,2);
+      Vector2d t0uw = new Vector2d(t0s.get(0),t0s.get(1));
+      Vector2d t1uw = new Vector2d(t1s.get(0),t1s.get(1));
+      Vector2d t2uw = new Vector2d(t2s.get(0),t2s.get(1));
+      Vector2d t0 = t0uw.mul(alpha);
+      Vector2d t1 = t1uw.mul(beta);
+      Vector2d t2 = t2uw.mul(gamma);
+      texture = t0.add(t1).add(t2);
     }
     else texture = null;
-    
-    //calculate location
-    Vector3d loc = rayIn.origin.addMultiple(t, rayIn.direction);
-    
+
+    // calculate location
+    Vector3d loc = rayIn.origin.clone().addMultiple(t, rayIn.direction);
+
     //create outrecord
     outRecord.location.set(loc);
     outRecord.normal.set(normal.normalize());
-    outRecord.texCoords.set(texture);
+    if (texture != null) {
+      outRecord.texCoords.set(texture);
+    }
     outRecord.surface = this;
     outRecord.t = t;
-    
+
     return true;
   }
 
