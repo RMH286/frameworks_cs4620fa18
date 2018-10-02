689aca679e221c3cc0420ab66a7764308786d763
Ryan Hornung <rmh286@cornell.edu>
2018-10-02 10:45:05 -0400

    proper mouse tracking

--- a3/src/manip/TranslationManipulator.java
+++ a3/src/manip/TranslationManipulator.java
@@ -31,7 +31,49 @@ public class TranslationManipulator extends Manipulator {
     //   for both X and Y. That is, the origin is the center of the screen, (-1,-1) is the bottom left
     //   corner of the screen, and (1, 1) is the top right corner of the screen.
     // TODO(ryan): calculate proper delta
-    float delta = (curMousePos.x - lastMousePos.x);
+    // TODO: fix projection of mouse pos into world coordinates
+    Vector3 lastWorldPos = viewProjection.mulPos(new Vector3(lastMousePos.x, lastMousePos.y, 0));
+    Vector3 curWorldPos = viewProjection.mulPos(new Vector3(curMousePos.x, curMousePos.y, 0));
+
+    Vector3 manipulatorDir = new Vector3(0);
+    Vector3 manipulatorOrigin = new Vector3(0);
+
+    Vector3 negZ = new Vector3(0, 0, -1);
+
+    Vector3 planeNormal = new Vector3(0);
+
+    switch(this.axis) {
+      case X:
+        manipulatorDir.x = 1;
+        break;
+      case Y:
+        manipulatorDir.y = 1;
+        break;
+      case Z:
+        manipulatorDir.z = 1;
+        break;
+    }
+
+    viewProjection.mulDir(manipulatorDir);
+    viewProjection.mulPos(manipulatorOrigin);
+
+    manipulatorDir.normalize();
+
+    planeNormal.set(manipulatorDir.clone().cross(negZ).cross(manipulatorDir));
+
+    float lastT = manipulatorOrigin.dot(planeNormal) / lastWorldPos.dot(planeNormal);
+    float curT = manipulatorOrigin.dot(planeNormal) / curWorldPos.dot(planeNormal);
+
+    lastWorldPos.mul(lastT);
+    curWorldPos.mul(curT);
+
+    lastWorldPos.sub(manipulatorOrigin);
+    curWorldPos.sub(manipulatorOrigin);
+
+    lastT = lastWorldPos.dot(manipulatorDir);
+    curT = curWorldPos.dot(manipulatorDir);
+
+    float delta = (curT - lastT);
     Vector3 v = new Vector3(0);
     switch(this.axis) {
       case X:
@@ -44,6 +86,7 @@ public class TranslationManipulator extends Manipulator {
         v.z = delta;
         break;
     }
+
     Matrix4 translation = Matrix4.createTranslation(v);
     this.reference.translation.mulBefore(translation);
 
