58a9c2ebfc80b5147d7bcfdcbf498c1510a61cce
Ryan Hornung <rmh286@cornell.edu>
2018-12-04 23:43:28 -0500

    working point light integrator

--- a7/src/ray2/integrator/PointLightIntegrator.java
+++ a7/src/ray2/integrator/PointLightIntegrator.java
@@ -19,65 +19,65 @@ import ray2.surface.Surface;
  */
 public class PointLightIntegrator extends Integrator {
 
-	/*
-	 * The illumination algorithm is:
-	 *   for each point light in the scene:
-	 *     compute the light direction and distance
-	 *     evaluate the BRDF
-	 *     add a contribution to the reflected radiance due to that source
-	 *
-	 * @see ray2.integrator.Integrator#shade(egl.math.Colord, ray2.Scene, ray2.Ray, ray2.IntersectionRecord, int)
-	 */
-	@Override
-	public void shade(Colord outRadiance, Scene scene, Ray ray, IntersectionRecord iRec, int depth) {
-		// TODO#A7: Calculate outRaidance at current shading point.
-		//outRadiance.add(new Colord(0,0,0));
-		for(int i = 0; i<scene.getLights().size(); i++) {
-			LightSamplingRecord lRec = new LightSamplingRecord();
-			scene.getLights().get(i).sample(lRec, iRec.location);
-			if(lRec.probability == 1.0) {
-				PointLight light = (PointLight)scene.getLights().get(i);
-				if((isShadowed(scene,iRec.location,light.position))) {
-					Colord cont = new Colord();
-					iRec.surface.getBSDF().eval(ray.direction.negate().normalize(),lRec.direction.normalize(), iRec.normal, cont);
-					if(iRec.normal.normalize().dot(lRec.direction.normalize())>=0) {
-						cont.mul((iRec.normal.normalize().dot(lRec.direction.normalize()))).mul(lRec.attenuation).mul(light.getIntensity());
-						cont.toColor();
-						outRadiance.add(cont);
-					}
-				}
-				outRadiance.toColor();
-			}
-		}
-		
-		
-	}
+  /*
+   * The illumination algorithm is:
+   *   for each point light in the scene:
+   *     compute the light direction and distance
+   *     evaluate the BRDF
+   *     add a contribution to the reflected radiance due to that source
+   *
+   * @see ray2.integrator.Integrator#shade(egl.math.Colord, ray2.Scene, ray2.Ray, ray2.IntersectionRecord, int)
+   */
+  @Override
+  public void shade(Colord outRadiance, Scene scene, Ray ray, IntersectionRecord iRec, int depth) {
+    // TODO#A7: Calculate outRaidance at current shading point.
+    //outRadiance.add(new Colord(0,0,0));
+    for (int i = 0; i < scene.getLights().size(); i++) {
+      if (scene.getLights().get(i) instanceof PointLight) {
+        PointLight light = (PointLight) scene.getLights().get(i);
+        LightSamplingRecord lRec = new LightSamplingRecord();
+        light.sample(lRec, iRec.location);
+        if (!isShadowed(scene, iRec.location, light.position)) {
+          Colord cont = new Colord();
+          iRec.surface.getBSDF().eval(ray.direction.clone().negate().normalize(),
+              lRec.direction.clone().normalize(),
+              iRec.normal,
+              cont);
+          cont.mul(iRec.normal.normalize().dot(lRec.direction.clone().normalize()));
+          cont.mul(lRec.attenuation);
+          cont.mul(light.getIntensity());
+          outRadiance.add(cont);
+        }
+      }
+    }
 
-	/**
-	 * A utility method to check if there is any surface between the given intersection
-	 * point and the given light.
-	 *
-	 * @param scene The scene in which the surface exists.
-	 * @param light A light in the scene.
-	 * @param iRec The intersection point on a surface.
-	 * @param shadowRay A ray that is set to point from the intersection point towards
-	 * the given light.
-	 * @return true if there is any surface between the intersection point and the light;
-	 * false otherwise.
-	 */
-	protected boolean isShadowed(Scene scene, Vector3d shadingPoint, Vector3d lightPosition) {
+  }
 
-		Ray shadowRay = new Ray();
+  /**
+   * A utility method to check if there is any surface between the given intersection
+   * point and the given light.
+   *
+   * @param scene The scene in which the surface exists.
+   * @param light A light in the scene.
+   * @param iRec The intersection point on a surface.
+   * @param shadowRay A ray that is set to point from the intersection point towards
+   * the given light.
+   * @return true if there is any surface between the intersection point and the light;
+   * false otherwise.
+   */
+  protected boolean isShadowed(Scene scene, Vector3d shadingPoint, Vector3d lightPosition) {
 
-		// Setup the shadow ray to start at surface and end at light
-		shadowRay.origin.set(shadingPoint);
-		shadowRay.direction.set(lightPosition).sub(shadingPoint);
+    Ray shadowRay = new Ray();
 
-		// Set the ray to end at the light
-		shadowRay.makeOffsetSegment(shadowRay.direction.len());
-		shadowRay.direction.normalize();
+    // Setup the shadow ray to start at surface and end at light
+    shadowRay.origin.set(shadingPoint);
+    shadowRay.direction.set(lightPosition).sub(shadingPoint);
 
-		return scene.getAnyIntersection(shadowRay);
-	}
+    // Set the ray to end at the light
+    shadowRay.makeOffsetSegment(shadowRay.direction.len());
+    shadowRay.direction.normalize();
+
+    return scene.getAnyIntersection(shadowRay);
+  }
 
 }
